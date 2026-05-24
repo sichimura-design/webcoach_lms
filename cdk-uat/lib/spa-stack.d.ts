b4dd811978e8565ff76616105c7d8e4d1b67713f
root <root@LAPTOP-QU1QL5IJ.localdomain>
2026-05-24 23:25:53 +0900

    不要なファイルを削除

--- cdk-uat/lib/spa-stack.d.ts
+++
@@ -1,8 +0,0 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-export interface UatSpaStackProps extends cdk.StackProps {
-    readonly envName: string;
-}
-export declare class UatSpaStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props: UatSpaStackProps);
-}
